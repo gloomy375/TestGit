e570f9f3cf2ef0b8938417bb1140fae13334ff5d
gloomy375 <dimosnesterenko@gmail.com>
2023-09-26 19:12:38 +0300

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by MacBook on 26.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
